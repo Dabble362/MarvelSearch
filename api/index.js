191c72d9738d571642b404ed8bd6dff54fc0b1cb
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-01-30 12:30:18 -0500

    rearrange index.js for best practicies

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+
+// API test route and Express initialization
+var testAPIRouter = require("./routes/testAPI");
 const app = express();
-const PORT = 9000;
+app.use("/testAPI", testAPIRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const PORT = 9000;
 app.listen(PORT, () => {
   console.log(`server is up and running on port ${PORT}`);
 });
