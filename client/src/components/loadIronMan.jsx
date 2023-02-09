8293f16bd55850baeaeda402b7a08edf6e04db01
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-02-09 13:12:47 -0500

    added .env variables

--- client/src/components/loadIronMan.jsx
+++ client/src/components/loadIronMan.jsx
@@ -9,27 +9,23 @@ class IronMan extends React.Component {
   };
 
   componentDidMount() {
-    let rEACT_APP_PUB_KEY = "00753159646691ae2094a5008957d748";
-    let rEACT_APP_PRI_KEY = "c2bbc050c357f2e24c82b51d5079809c76bb6e58";
-    let rEACT_APP_BASE_URL = "https://gateway.marvel.com/v1/public";
-    let searchQuery = "iron%20man";
-    let ts = new Date().getTime();
-    let message = ts + rEACT_APP_PRI_KEY + rEACT_APP_PUB_KEY;
+    const searchQuery = "iron%20man";
+    const ts = new Date().getTime();
+    const message =
+      ts + process.env.REACT_APP_PRI_KEY + process.env.REACT_APP_PUB_KEY;
     var hashedValue = md5(message);
 
     fetch(
-      `${rEACT_APP_BASE_URL}/characters?name=${searchQuery}&ts=${ts}&apikey=${rEACT_APP_PUB_KEY}&hash=${hashedValue}`
+      `${process.env.REACT_APP_BASE_URL}/characters?name=${searchQuery}&ts=${ts}&apikey=${process.env.REACT_APP_PUB_KEY}&hash=${hashedValue}`
     )
       .then((res) => res.json())
       .then((hero) => {
-        console.log(hero.data.results[0]);
         this.setState({
           name: hero.data.results[0].name,
           description: hero.data.results[0].description,
           imagePath: hero.data.results[0].thumbnail.path,
         });
       })
-
       .catch((err) => console.log(err));
   }
 
