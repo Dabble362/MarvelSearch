b303b21595b0c11f8aba8d9fcce47de3ddd48058
Daniel Bennett <danielandrew362@gmail.com>
2023-02-13 15:49:11 -0500

    add code comments in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 import IronMan from "./components/loadIronMan";
 import Header from "./components/header";
 import NavBar from "./components/navigation";
+
 class App extends Component {
+  // const [selectedAvenger, setSelectedAvenger] = useState("");
+  // const [showDetails, setShowDetails] = useState(false);
+  // const [avengerData, setAvengerData] = useState([]);
+
+
   render() {
     return (
       <div className="App">
