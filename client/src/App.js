d8db643d048d10ffd3e0b5219e7fb35670944513
Daniel Bennett <danielandrew362@gmail.com>
2023-02-13 15:27:59 -0500

    Marvel get (#2)
    Add Navbar component - fetches current Marvel API images for six marvel characters
    
    add IronMan component - base herocard component that fetches Name, Larger portrait, and description of iron man

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,15 @@
 import { Component } from "react";
 import "./App.css";
-
+import IronMan from "./components/loadIronMan";
+import Header from "./components/header";
+import NavBar from "./components/navigation";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
-      .catch((err) => err);
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>Hello There</h1>
-        <p>{this.state.apiResponse}</p>
+        <Header />
+        <NavBar />
+        <IronMan />
       </div>
     );
   }
---
+++ client/src/components/navigation.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import md5 from "md5";
+
+class NavBar extends React.Component {
+  state = {
+    ironMan: "",
+    captainAmerica: "",
+    thor: "",
+    blackWidow: "",
+    theHulk: "",
+    hawkeye: "",
+  };
+
+  componentDidMount() {
+    const ts = new Date().getTime();
+    const message =
+      ts + process.env.REACT_APP_PRI_KEY + process.env.REACT_APP_PUB_KEY;
+    var hashedValue = md5(message);
+
+    const heroes = {
+      "ironMan": "iron man",
+      "captainAmerica": "captain america",
+      "thor": "thor",
+      "blackWidow": "black widow",
+      "theHulk": "hulk",
+      "hawkeye": "hawkeye",
+    }
+
+    Object.entries(heroes).forEach(([stateKey, heroName]) => {
+      fetch(
+        `${process.env.REACT_APP_BASE_URL}/characters?name=${heroName}&ts=${ts}&apikey=${process.env.REACT_APP_PUB_KEY}&hash=${hashedValue}`
+      )
+        .then((res) => res.json())
+        .then((hero) => {
+          this.setState({
+            [stateKey]: hero.data.results[0].thumbnail.path,
+          });
+        })
+        .catch((err) => console.log(err));
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul className="heroList">
+          {Object.keys(this.state).map((hero) => {
+            return (
+              <li>
+                <img
+                  alt={hero}
+                  src={`${this.state[hero]}/portrait_medium.jpg`}
+                ></img>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
