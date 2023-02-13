e83ce37beaac250970f69f146402d565800c480b
Daniel Bennett <danielandrew362@gmail.com>
2023-02-13 16:12:19 -0500

    add code that should be stashed

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,43 @@ import IronMan from "./components/loadIronMan";
 import Header from "./components/header";
 import NavBar from "./components/navigation";
 
-class App extends Component {
-  // const [selectedAvenger, setSelectedAvenger] = useState("");
-  // const [showDetails, setShowDetails] = useState(false);
-  // const [avengerData, setAvengerData] = useState([]);
+function App() {
+  const [selectedAvenger, setSelectedAvenger] = useState("");
+  const [showDetails, setShowDetails] = useState(false);
+  const [avengerData, setAvengerData] = useState([]);
 
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <NavBar />
-        <IronMan />
-      </div>
-    );
+  const handleImageClick = (image) => {
+    setSelectedAvenger(image);
+    setShowDetails(true);
   }
+
+  const handleClosedDetails = () => {
+    setShowDetails(false);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <NavBar onImageClick={handleImageClick} imagesData={imagesData} />
+      {showDetails && (
+        <IronMan
+          image={selectedAvenger}
+          onCloseDetails={handleClosedDetails}
+        />
+      )}
+      <IronMan />
+    </div>
+  );
+}
+
+function ImageDetails({ image, onCloseDetails }) {
+  return (
+    <div>
+      <h1>{image.name}</h1>
+      <p>{image.description}</p>
+      <button onClick={onCloseDetails}>Close</button>
+    </div>
+  );
 }
 
 export default App;
