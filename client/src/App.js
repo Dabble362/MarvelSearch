cd1987618cf8c20fdcd6298007227d19618ab149
Jed Duffey <jedduffey@gmail.com>
2023-02-14 11:40:48 -0500

    Capitalize/standardize component file names

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 
-import IronMan from "./components/loadIronMan";
-import Header from "./components/header";
-import NavBar from "./components/navigation";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import IronMan from "./components/IronMan";
 
 export default function App() {
   const handleImageClick = (heroName) => {
