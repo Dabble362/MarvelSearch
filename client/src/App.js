503474893f5c72e62cb96cd9aaf6311d385f5ffd
Jed Duffey <jedduffey@gmail.com>
2023-02-14 11:08:58 -0500

    Comment out everything that is breaking the app from the last commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,47 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./App.css";
 import IronMan from "./components/loadIronMan";
 import Header from "./components/header";
 import NavBar from "./components/navigation";
 
 function App() {
-  const [selectedAvenger, setSelectedAvenger] = useState("");
-  const [showDetails, setShowDetails] = useState(false);
-  const [avengerData, setAvengerData] = useState([]);
+//   const [selectedAvenger, setSelectedAvenger] = useState("");
+//   const [showDetails, setShowDetails] = useState(false);
+//   const [avengerData, setAvengerData] = useState([]);
 
   const handleImageClick = (image) => {
-    setSelectedAvenger(image);
-    setShowDetails(true);
+    // setSelectedAvenger(image);
+    // setShowDetails(true);
   }
 
   const handleClosedDetails = () => {
-    setShowDetails(false);
+    // setShowDetails(false);
   }
 
   return (
     <div className="App">
       <Header />
-      <NavBar onImageClick={handleImageClick} imagesData={imagesData} />
-      {showDetails && (
+      {/* <NavBar onImageClick={handleImageClick} imagesData={imagesData} /> */}
+      <NavBar onImageClick={handleImageClick}/>
+      {/* {showDetails && (
         <IronMan
           image={selectedAvenger}
           onCloseDetails={handleClosedDetails}
         />
-      )}
+      )} */}
       <IronMan />
     </div>
   );
 }
 
-function ImageDetails({ image, onCloseDetails }) {
-  return (
-    <div>
-      <h1>{image.name}</h1>
-      <p>{image.description}</p>
-      <button onClick={onCloseDetails}>Close</button>
-    </div>
-  );
-}
+// function ImageDetails({ image, onCloseDetails }) {
+//   return (
+//     <div>
+//       <h1>{image.name}</h1>
+//       <p>{image.description}</p>
+//       <button onClick={onCloseDetails}>Close</button>
+//     </div>
+//   );
+// }
 
 export default App;
